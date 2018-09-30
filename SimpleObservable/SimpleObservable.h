750a26bc1888511791eab94f6fefa36b7704b32b
thelowlypeon <me@petercompernolle.com>
2018-09-30 16:57:15 -0500

    Initial Commit

---
+++ SimpleObservable/SimpleObservable.h
@@ -0,0 +1,19 @@
+//
+//  SimpleObservable.h
+//  SimpleObservable
+//
+//  Created by Peter Compernolle on 9/30/18.
+//  Copyright © 2018 Peter Compernolle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleObservable.
+FOUNDATION_EXPORT double SimpleObservableVersionNumber;
+
+//! Project version string for SimpleObservable.
+FOUNDATION_EXPORT const unsigned char SimpleObservableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleObservable/PublicHeader.h>
+
+
