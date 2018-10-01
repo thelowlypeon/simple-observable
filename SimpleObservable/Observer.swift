88f04c609ec745438d0d8dd16c89ec151aa8b823
thelowlypeon <me@petercompernolle.com>
2018-10-01 07:19:12 -0500

    adds callbacks to observers

--- SimpleObservable/Observer.swift
+++ SimpleObservable/Observer.swift
@@ -9,17 +9,28 @@
 import Foundation
 
 public typealias PropertyObserverIdentifier = UUID
+public typealias PropertyChangedCallback<T> = ((T) -> Void)
 
 public class Observer<T> {
     public let identifier = PropertyObserverIdentifier()
 
     private(set) var currentValue: T
+    private var onNextCallbacks = [PropertyChangedCallback<T>]()
 
     public init(initialValue: T) {
         self.currentValue = initialValue
     }
 
+    public func onNext(_ callback: @escaping PropertyChangedCallback<T>) -> Self {
+        onNextCallbacks.append(callback)
+        callback(currentValue)
+        return self
+    }
+
     public func send(_ value: T) {
+        for callback in onNextCallbacks {
+            callback(value)
+        }
         currentValue = value
     }
 }
---
+++ SimpleObservableTests/ObserverCallbacksTests.swift
@@ -0,0 +1,53 @@
+//
+//  ObserverCallbacksTests.swift
+//  SimpleObservableTests
+//
+//  Created by Peter Compernolle on 10/1/18.
+//  Copyright © 2018 Peter Compernolle. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleObservable
+
+class ObserverCallbacksTests: XCTestCase {
+    let initialValue = "initial value"
+    let newValue = "new value"
+    var observer: Observer<String>!
+
+    override func setUp() {
+        observer = Observer<String>(initialValue: initialValue)
+    }
+
+    func testCallbackIsCalledWithInitialValue() {
+        let initialValueExpectation = expectation(description: "observer callback was called with initial value")
+        let _ = observer.onNext({(value) in
+            if value == self.initialValue {
+                initialValueExpectation.fulfill()
+            }
+        })
+        observer.send(newValue)
+        waitForExpectations(timeout: 1)
+    }
+
+    func testCallbackIsCalledWithNewValueWhenValueIsChanged() {
+        let newValueExpectation = expectation(description: "observer callback was called with new value")
+        let _ = observer.onNext({(value) in
+            if value == self.newValue {
+                newValueExpectation.fulfill()
+            }
+        })
+        observer.send(newValue)
+        waitForExpectations(timeout: 1)
+    }
+
+    func testObserverWithMultipleCallbacks() {
+        let expectationCallback1 = expectation(description: "first callback was called")
+        let expectationCallback2 = expectation(description: "second callback was called")
+        let _ = observer.onNext({(value) in
+            expectationCallback1.fulfill()
+        }).onNext({(value) in
+            expectationCallback2.fulfill()
+        })
+        waitForExpectations(timeout: 1)
+    }
+}
