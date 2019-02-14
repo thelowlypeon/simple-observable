90ad5df824718a02c1f2978ba3434b6248481d32
thelowlypeon <me@petercompernolle.com>
2019-02-14 13:39:18 -0600

    updates playground to use a real UIViewController, logs deinit

--- SimpleObservable.playground/Contents.swift
+++ SimpleObservable.playground/Contents.swift
@@ -2,6 +2,7 @@
 // It builds a crazy simple app that allows you to change people's names in MVVM.
 
 import UIKit
+import PlaygroundSupport
 import SimpleObservable
 
 class Person {
@@ -15,8 +16,19 @@ class Person {
         self._lastName = Property<String>(lastName)
     }
 
-    func isBeatle() -> Bool {
-        return ["John", "Paul", "George", "Ringo"].contains(firstName)
+    func jumbleName() {
+        firstName = jumbleString(firstName)
+        lastName = jumbleString(lastName)
+    }
+
+    private func jumbleString(_ input: String) -> String {
+        var characters = Array(input)
+        let replace = Int.random(in: 0..<characters.count)
+        let with = Int.random(in: 0..<characters.count)
+        let replacement = characters[replace]
+        characters[replace] = characters[with]
+        characters[with] = replacement
+        return String(characters)
     }
 
     deinit {
@@ -46,13 +58,11 @@ class UserViewModel {
     let _personNameText = Property<String?>()
 
     // MARK -- observe changes to the person
-    private let _person = Property<Person?>()
+    private let _selectedPerson = Property<Person?>()
     private var _personListener: Listener<Person?>?
 
     init() {
-        _personListener = _person.filter(nil, {(person) in
-            return person?.isBeatle() ?? true // only update if the person is a beatle or nil
-        }).on(next: {[weak self] (person) in
+        _personListener = _selectedPerson.on(next: {[weak self] (person) in
             guard let self = self else { return }
             if let person = person {
                 person._firstName.on(next: {[weak self] _ in self?.updateNameString() })
@@ -64,7 +74,7 @@ class UserViewModel {
     }
 
     private func updateNameString() {
-        if let person = self.person {
+        if let person = self.selectedPerson {
             personNameText = "\(person.firstName) \(person.lastName)"
         } else {
             personNameText = nil
@@ -78,9 +88,9 @@ class UserViewModel {
 }
 
 extension UserViewModel {
-    var person: Person? {
-        get { return _person.value }
-        set { _person.value = newValue }
+    var selectedPerson: Person? {
+        get { return _selectedPerson.value }
+        set { _selectedPerson.value = newValue }
     }
     private(set) var personNameText: String? {
         get { return _personNameText.value }
@@ -88,70 +98,133 @@ extension UserViewModel {
     }
 }
 
-class UserView {
-    private var people: [Person]
-    private let viewModel: UserViewModel
-    private var personNameTextObserver: Listener<String?>?
+class PeopleViewModel {
+    let _people = Property<[Person]>([])
+    var people: [Person] { return _people.value }
 
-    var nameLabel: UILabel!
-    var changePersonButton: UIButton!
-    var changePersonNameButton: UIButton!
+    let _title = Property<String>("Loading...")
+    var title: String { return _title.value }
 
-    init(people: [Person]) {
-        self.nameLabel = UILabel(frame: CGRect.zero)
-        self.people = people
-        self.viewModel = UserViewModel()
+    init() {
+        fetchPeople()
     }
 
-    func fetchPeople(completionHandler: (() -> Void)?) {
-        DispatchQueue.global(qos: .background).async {
-            self.people = [
-                Person(firstName: "John", lastName: "L"),
-                Person(firstName: "Paul", lastName: "M"),
-                Person(firstName: "George", lastName: "H"),
-                Person(firstName: "Ringo", lastName: "S"),
-                Person(firstName: "Peter", lastName: "C")
+    private func fetchPeople() {
+        //async to simulate remote request
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {[weak self] in
+            guard let self = self else { return }
+            self._people.value = [
+                Person(firstName: "John", lastName: "Lennon"),
+                Person(firstName: "Paul", lastName: "McCartney"),
+                Person(firstName: "George", lastName: "Harrison"),
+                Person(firstName: "Ringo", lastName: "Starr"),
+                Person(firstName: "The Lowly", lastName: "Peon")
             ]
-            completionHandler?()
+            self._title.value = "\(self.people.count) People"
         }
     }
 
-    func viewWillAppear() {
+    deinit { print("deinitializing PeopleViewModel") }
+}
+
+class PeopleViewController: UITableViewController {
+    private var viewModel: PeopleViewModel!
+    let reuseIdentifier = "person"
+
+    override func viewWillAppear(_ animated: Bool) {
+        if viewModel == nil {
+            viewModel = PeopleViewModel()
+        }
+        viewModel._people.on(next: {[weak self]_ in
+            self?.tableView.reloadData()
+        })
+        viewModel._title.on(next: {[weak self](title) in self?.title = title })
+        super.viewWillAppear(animated)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.people.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell!
+        cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier)
+
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: reuseIdentifier)
+            cell.accessoryType = .disclosureIndicator
+        }
+
+        let person = viewModel.people[indexPath.row]
+        cell.textLabel?.text = "\(person.firstName) \(person.lastName)"
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let person = self.viewModel.people[indexPath.row]
+        let vc = UserViewController()
+        vc.displayPerson(person)
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+
+    deinit { print("deinitializing PeopleViewController") }
+}
+
+class UserViewController: UIViewController {
+    private let viewModel = UserViewModel()
+    private var personNameTextObserver: Listener<String?>?
+
+    lazy var nameLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 0, y: 200, width: 375, height: 20))
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    lazy var jumblePersonNameButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 300, width: 375, height: 20))
+        button.setTitle("Jumble!", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.addTarget(self, action: #selector(jumblePersonName), for: .touchUpInside)
+        return button
+    }()
+
+    override func loadView() {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 375, height: 668))
+        view.backgroundColor = .white
+        view.addSubview(self.jumblePersonNameButton)
+        view.addSubview(self.nameLabel)
+        self.view = view
+    }
+
+    func displayPerson(_ person: Person) {
+        viewModel.selectedPerson = person
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
         // want to see how important it is to use [weak self] in callbacks like this?
         // try removing it and see what _doesn't_ get released
         personNameTextObserver = viewModel._personNameText
-            .distinct()
+            //.distinct()
             .on(next: {[weak self](text) in
                 self?.nameLabel.text = text
-                self?.render()
             })
+        super.viewWillAppear(animated)
+    }
+
+    @objc func jumblePersonName() {
+        viewModel.selectedPerson?.jumbleName()
     }
 
     func viewWillDisappear() {
         personNameTextObserver?.stopObserving()
     }
 
-    func chooseRandomPerson() {
-        self.viewModel.person = people.randomElement()
-    }
-
-    func render() {
-        print("View shows: \(nameLabel.text ?? "nil")")
-    }
-
     deinit { print("deinitializing UserView") }
 }
 
-var view: UserView? = UserView(people: [])
-view!.viewWillAppear()
-view!.fetchPeople {
-    for _ in 0...20 {
-        view!.chooseRandomPerson()
-    }
-}
 
-DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .seconds(2)) {
-    view!.viewWillDisappear()
-    view = nil
-    print("reference count: \(SimpleObservableReferenceManager.shared.getCount())")
-}
+let peopleViewController = PeopleViewController()
+let navigationController = UINavigationController(rootViewController: peopleViewController)
+PlaygroundPage.current.liveView = navigationController
+PlaygroundPage.current.needsIndefiniteExecution = true
